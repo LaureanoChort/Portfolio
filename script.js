565b018f05ee09f7469ae2f82221f5b325711eb8
Laureano <chortlaureano@gmail.com>
2021-08-12 08:20:25 -0300

    V1

---
+++ script.js
@@ -0,0 +1,75 @@
+//Menu
+let menuBtn = document.querySelector(".fa-bars");
+
+menuBtn.addEventListener("click", ()=> {
+	let nav = document.querySelector(".nav");
+	let navBackgr = document.querySelector(".nav-background");
+	let closeBtn = document.querySelector(".close-btn");
+
+	nav.classList.add("show");
+	navBackgr.classList.add("show");	
+	closeBtn.addEventListener("click", ()=> {
+		nav.classList.remove("show");
+		navBackgr.classList.remove("show");
+	})
+
+	navBackgr.addEventListener("click", ()=> {
+		nav.classList.remove("show");
+		navBackgr.classList.remove("show");
+	})
+
+	let menuItems = document.querySelectorAll(".menu-item");
+	for (let i = 0; i < menuItems.length; i++) {
+		menuItems[i].addEventListener("click", ()=> {
+			nav.classList.remove("show");
+			navBackgr.classList.remove("show");
+		})
+	}
+})
+
+//Typing effect
+let txtIndex = 0;
+
+const type = () => {
+	let txt = document.querySelector(".text-2")
+	let txtContent = "Front-end developer";
+	let cursor = document.querySelector(".cursor");
+	cursor.classList.add("typing");
+	cursor.style.display = "inline-block";
+
+	if(txtIndex < txtContent.length) {
+		txt.textContent += txtContent.charAt(txtIndex);
+		txtIndex++;
+		setTimeout(type,150);
+	} else {
+		cursor.classList.remove("typing");
+		setTimeout(() => cursor.remove(),3000);
+	}
+}
+
+document.addEventListener("DOMContentLoaded", ()=> {
+	setTimeout(type,1200)
+})
+
+
+//Scroll
+const show = () => {
+    let sections = document.querySelectorAll('.hidden');
+
+    for (let i = 0; i < sections.length; i++) {
+
+    	let windowHeight = window.innerHeight;
+        let sectionTop = sections[i].getBoundingClientRect().top; 
+        let sectionPoint = 50;
+
+        if(sectionTop < windowHeight - sectionPoint) {
+            sections[i].classList.add("active");
+        }
+    }
+}
+
+window.addEventListener('scroll', ()=> {
+	show();
+	let upBtn = document.querySelector(".up-btn");
+	upBtn.classList.toggle("sticky", window.scrollY > 0);
+})
