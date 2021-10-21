ad2ff3c3006c07c818cba20308c6341f61e52517
LaureanoChort <chortlaureano@gmail.com>
2021-10-21 20:45:34 -0300

    V2

--- script.js
+++ script.js
@@ -1,75 +1,67 @@
 //Menu
 let menuBtn = document.querySelector(".fa-bars");
 
-menuBtn.addEventListener("click", ()=> {
-	let nav = document.querySelector(".nav");
-	let navBackgr = document.querySelector(".nav-background");
-	let closeBtn = document.querySelector(".close-btn");
+menuBtn.addEventListener("click", () => {
+  let nav = document.querySelector(".nav");
+  let navBackgr = document.querySelector(".nav-background");
+  let closeBtn = document.querySelector(".close-btn");
 
-	nav.classList.add("show");
-	navBackgr.classList.add("show");	
-	closeBtn.addEventListener("click", ()=> {
-		nav.classList.remove("show");
-		navBackgr.classList.remove("show");
-	})
+  nav.classList.add("show");
+  navBackgr.classList.add("show");
+  closeBtn.addEventListener("click", () => {
+    nav.classList.remove("show");
+    navBackgr.classList.remove("show");
+  });
 
-	navBackgr.addEventListener("click", ()=> {
-		nav.classList.remove("show");
-		navBackgr.classList.remove("show");
-	})
+  navBackgr.addEventListener("click", () => {
+    nav.classList.remove("show");
+    navBackgr.classList.remove("show");
+  });
 
-	let menuItems = document.querySelectorAll(".menu-item");
-	for (let i = 0; i < menuItems.length; i++) {
-		menuItems[i].addEventListener("click", ()=> {
-			nav.classList.remove("show");
-			navBackgr.classList.remove("show");
-		})
-	}
-})
+  let menuItems = document.querySelectorAll(".menu-item");
+  for (let i = 0; i < menuItems.length; i++) {
+    menuItems[i].addEventListener("click", () => {
+      nav.classList.remove("show");
+      navBackgr.classList.remove("show");
+    });
+  }
+});
 
 //Typing effect
 let txtIndex = 0;
 
 const type = () => {
-	let txt = document.querySelector(".text-2")
-	let txtContent = "Front-end developer";
-	let cursor = document.querySelector(".cursor");
-	cursor.classList.add("typing");
-	cursor.style.display = "inline-block";
+  let txt = document.querySelector(".text-2");
+  let txtContent = "Front-end developer";
 
-	if(txtIndex < txtContent.length) {
-		txt.textContent += txtContent.charAt(txtIndex);
-		txtIndex++;
-		setTimeout(type,150);
-	} else {
-		cursor.classList.remove("typing");
-		setTimeout(() => cursor.remove(),3000);
-	}
-}
+  if (txtIndex < txtContent.length) {
+    txt.textContent += txtContent.charAt(txtIndex);
+    txtIndex++;
+    setTimeout(type, 150);
+  }
+};
 
-document.addEventListener("DOMContentLoaded", ()=> {
-	setTimeout(type,1200)
-})
-
+document.addEventListener("DOMContentLoaded", () => {
+  setTimeout(type, 1200);
+});
 
 //Scroll
 const show = () => {
-    let sections = document.querySelectorAll('.hidden');
+  let sections = document.querySelectorAll(".hidden");
 
-    for (let i = 0; i < sections.length; i++) {
+  for (let i = 0; i < sections.length; i++) {
+    let windowHeight = window.innerHeight;
+    let sectionTop = sections[i].getBoundingClientRect().top;
+    let sectionPoint = 50;
 
-    	let windowHeight = window.innerHeight;
-        let sectionTop = sections[i].getBoundingClientRect().top; 
-        let sectionPoint = 50;
-
-        if(sectionTop < windowHeight - sectionPoint) {
-            sections[i].classList.add("active");
-        }
+    if (sectionTop < windowHeight - sectionPoint) {
+      sections[i].classList.add("active");
     }
-}
+  }
+};
 
-window.addEventListener('scroll', ()=> {
-	show();
-	let upBtn = document.querySelector(".up-btn");
-	upBtn.classList.toggle("sticky", window.scrollY > 0);
-})
+window.addEventListener("scroll", () => {
+  show();
+  let upBtn = document.querySelector(".up-btn");
+  upBtn.classList.toggle("sticky", window.scrollY > 0);
+});
